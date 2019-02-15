3e0ff48bc511380fa451376869fcf1d60f01a056
Ranjitha Y <Ranjitha.Y@Lnttechservices.com>
2019-02-15 15:20:03 +0530

    function

---
+++ function.py
@@ -0,0 +1,10 @@
+
+#function definition is here 
+def python_training( name, age ):
+ 
+   print ("Name: ", name)
+   print ("Age ", age)
+   return;
+
+#now we can call funtion
+python_training( name="tom", age=20)
