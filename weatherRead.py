b75be05b1c9a0507b9cf24678f581fdc38b62fec
Heena Kousar R <HeenaKousar.R@Lnttechservices.com>
2019-02-15 12:31:54 +0530

    second_commit

---
+++ weatherRead.py
@@ -0,0 +1,8 @@
+import urllib.request, json
+import requests
+#url = " https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+
+with urllib.request.urlopen("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22") as url:
+	data = json.loads(url.read().decode())
+	print(data)
+
